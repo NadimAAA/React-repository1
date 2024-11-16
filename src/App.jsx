b4f41b2e392019ad6b9e0ab15ec386421e3bfc1e
Nadim Abou Ali <nadimabouali@mac.lan>
2024-11-16 18:01:54 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <h1 className="title">Lorem</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos sit eaque
+        accusamus, nulla expedita numquam distinctio? Obcaecati cum earum quae,
+        iste autem itaque sapiente ut, qui ex perferendis consectetur officiis?
+      </p>
+      <h1 className="title">ipsum</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt
+        laudantium odit nulla, est possimus ex. Eius voluptatibus, perferendis
+        maxime possimus temporibus aliquid alias? Consectetur nihil dolor
+        exercitationem tempora sint dicta!
+      </p>
+    </div>
+  );
+}
+
+export default App;
